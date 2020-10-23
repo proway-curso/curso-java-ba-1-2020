a6b8191b36dd2b7b9ec895b1e065e5a9bafa3974
Cleiton Capristano <cleiton.capristano@PROWAY.TREINA>
2020-10-23 20:53:49 -0300

    23-10-2020

--- src/orientaca_objetos/Main.java
+++ src/orientaca_objetos/Main.java
@@ -4,28 +4,25 @@ import java.util.Scanner;
 
 public class Main {
 	
-	private static Scanner sc = null;
-	private static Pessoa[] vetorPessoa = null;
+	private static Scanner sc = new Scanner(System.in);
 	
-	public static void main(String[] args) {
-		Main main = new Main();
-		sc = new Scanner(System.in);
-		
+	public static void main(String[] args) {	
 		Pessoa[] vetorPessoa = new Pessoa[2];
-		
-		for(int i = 0; i < vetorPessoa.length; i++) {
-			Pessoa pessoa = vetorPessoa[i];
+		for(int i = 0; i < vetorPessoa.length; i++) {			
 			System.out.println("Escreva o tipo da pessoa a se cadastrada");
 			System.out.println("1 - Pessoa Fisica");
 			System.out.println("2 - Pessoa Juridica");
 			int tipoPessoa = Integer.parseInt(sc.nextLine());
 			switch (tipoPessoa) {
 			case 1:
-				pessoa = new PessoaFisica();
-				//cadastroPessoaFisica(pessoa);
+				PessoaFisica pf = new PessoaFisica();
+				cadastroPessoaFisica(pf);
+				vetorPessoa[i] = pf;
 				break;
 			case 2:
-				pessoa = new PessoaJuridica();
+				PessoaJuridica pj = new PessoaJuridica();
+				cadastroPessoaJuridica(pj);
+				vetorPessoa[i] = pj;
 				break;
 
 			default:
@@ -33,12 +30,20 @@ public class Main {
 			}
 		}
 		
+		for(int i = 0; i < vetorPessoa.length; i++) {
+			System.out.println("Nome: " + vetorPessoa[i].getNome());
+			if(vetorPessoa[i] instanceof PessoaFisica) {
+				System.out.println("CPF: " + ((PessoaFisica)vetorPessoa[i]).getCpf());
+			} else {
+				System.out.println("CNPJ: " + ((PessoaJuridica)vetorPessoa[i]).getCnpj());
+			}
+		}
+		
 	}
 	
 	private static void cadastroPessoa(Pessoa p) {
 		System.out.println("Nome:");
-		p.setNome(sc.nextLine());
-		
+		p.setNome(sc.nextLine());		
 	}
 	
 	private static void cadastroPessoaFisica(PessoaFisica pf) {
@@ -50,9 +55,8 @@ public class Main {
 	
 	private static void cadastroPessoaJuridica(PessoaJuridica pj) {
 		cadastroPessoa(pj);
-		System.out.println("CPF:");
-		pj.setCnpj(Long.parseLong(sc.nextLine()));
-		
+		System.out.println("CNPJ:");
+		pj.setCnpj(Long.parseLong(sc.nextLine()));		
 	}
 
 }
